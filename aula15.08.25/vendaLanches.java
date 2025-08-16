e936c89693a384b7509111d0758dca56c5529198
vanderlinuxcode <vanderlitecnologia@gmail.com>
2025-08-16 15:20:04 -0300

    Atualização no Código
    Resumo da aquisição de lanche agora mostrando o que adquiriu e a quantidade.

--- aula15.08.25/vendaLanches.java
+++ aula15.08.25/vendaLanches.java
@@ -6,190 +6,239 @@ import java.util.Random;
 public class vendaLanches {
 
 	// método throws InterrupteDexception para simular o tempo de espera
-    public static void main(String[] args) throws InterruptedException {
-        Scanner pdv = new Scanner(System.in);
-        Random gerarComanda = new Random();
+	public static void main(String[] args) throws InterruptedException {
+		Scanner pdv = new Scanner(System.in);
+		Random gerarComanda = new Random();
 
-        // Gera número de comanda aleatório entre 100 e 999
-        int comanda = 100 + gerarComanda.nextInt(999);
+		// Gera número de comanda aleatório entre 100 e 999
+		int comanda = 100 + gerarComanda.nextInt(999);
 
-        // CARDÁPIO
-        System.out.println("  =============================================== ");
-        System.out.println("  |                 CARDÁPIO                     | ");
-        System.out.println("  =============================================== ");
-        System.out.println("  =  Hamburgueres ==== Refrescos ===== Fritura == ");
-        System.out.println("  = (01) X-Salada    (02) Suco     (03) Batata  = ");
-        System.out.println("  = (11) X-Egg       (21) Refri    (31) Palmito = ");
-        System.out.println("  = (12) X-Tudo      (22) Água     (32) Picles  = ");
-        System.out.println("  ===============================================\n");
+		// CARDÁPIO
+		System.out.println("  =============================================== ");
+		System.out.println("  |                 CARDÁPIO                     | ");
+		System.out.println("  =============================================== ");
+		System.out.println("  =  Hamburgueres ==== Refrescos ===== Fritura == ");
+		System.out.println("  = (01) X-Salada    (02) Suco     (03) Batata  = ");
+		System.out.println("  = (11) X-Egg       (21) Refri    (31) Palmito = ");
+		System.out.println("  = (12) X-Tudo      (22) Água     (32) Picles  = ");
+		System.out.println("  ===============================================\n");
 
-        // Entrada de dados
-        System.out.println("Número da Comanda: " + comanda);
-        System.out.print("Nome: ");
-        String nome = pdv.nextLine();
+		// Entrada de dados
+		System.out.println("Número da Comanda: " + comanda);
+		System.out.print("Nome: ");
+		String nome = pdv.nextLine();
 
-        System.out.print("Escolha Código Hamburguer: ");
-        int codigo = pdv.nextInt();
+		System.out.print("Escolha Código Hamburguer: ");
+		int codigo = pdv.nextInt();
 
-        // Caso a entrada não satisfaça a condição do Cardápio
-        if (codigo != 1 && codigo != 11 && codigo != 12) {
-            System.out.println("\n Código inválido. Operação cancelada.");
-            System.out.println("Por favor, reinicie o sistema e escolha um item válido do cardápio.");
-            pdv.close();
-            return;
-        }
+		// Caso a entrada não satisfaça a condição do Cardápio
+		if (codigo != 1 && codigo != 11 && codigo != 12) {
+			System.out.println("\n Código inválido. Operação cancelada.");
+			System.out.println("Por favor, reinicie o sistema e escolha um item válido do cardápio.");
+			pdv.close();
+			return;
+		}
 
-        if (codigo == 1) {
-            System.out.println("\nResumo da Comanda: ");
-            System.out.println("Comanda Nº: " + comanda);
-            System.out.println("Cliente: " + nome);
-            System.out.println("Pedido:");
-            System.out.print(" - Item " + codigo + " | ");
-            System.out.println("X-Salada");
-            System.out.print("Quantidade: ");
-            int quantidadeH = pdv.nextInt();
+		String nomeHamburguer = "";
+		String nomeBebida = "";
+		String nomeFritura = "";
+		int quantHamburg = 0;
+		int quantBebida = 0;
+		int quantFritura = 0;
 
-            System.out.println("Escolha Refresco 02 Suco 21 Refri 22 Água: ");
-            int refresco = pdv.nextInt();
+		if (codigo == 1) {
+			System.out.println("\nResumo da Comanda: ");
+			System.out.println("Comanda Nº: " + comanda);
+			System.out.println("Cliente: " + nome);
+			System.out.println("Pedido:");
+			System.out.print(" - Item " + codigo + " | ");
+			nomeHamburguer = "X-Salada";
+			System.out.printf("%s\n",nomeHamburguer);
+			System.out.print("Quantidade: ");
+			quantHamburg = pdv.nextInt();
+			
+			System.out.println("Escolha Refresco 02 Suco 21 Refri 22 Água: ");
+			int refresco = pdv.nextInt();
 
-            if (refresco == 2) {
-                System.out.println("Suco");
-                System.out.print("Quantidade: ");
-                int quantidadeS = pdv.nextInt();
-            } else if (refresco == 21) {
-                System.out.println("Refri");
-                System.out.print("Quantidade: ");
-                int quantidadeR = pdv.nextInt();
-            } else {
-                System.out.println("Água");
-                System.out.print("Quantidade: ");
-                int quantidadeA = pdv.nextInt();
-            }
+			if (refresco == 2) {
+				nomeBebida = "Suco";
+				System.out.printf("%s\n",nomeBebida);
+				System.out.print("Quantidade: ");
+				quantBebida = pdv.nextInt();
+				
+			} else if (refresco == 21) {
+				nomeBebida = "Refrigerante";
+				System.out.printf("%s\n",nomeBebida);
+				System.out.print("Quantidade: ");
+				quantBebida = pdv.nextInt();
+			
+			} else {
+				nomeBebida = "Água";
+				System.out.printf("%s\n",nomeBebida);
+				System.out.print("Quantidade: ");
+				quantBebida = pdv.nextInt();
+				
+			}
 
-            System.out.print("Escolha Fritura 03 Batata 31 Palmito 32 Picles: ");
-            int fritura = pdv.nextInt();
+			System.out.print("Escolha Fritura 03 Batata 31 Palmito 32 Picles: ");
+			int fritura = pdv.nextInt();
 
-            if (fritura == 3) {
-                System.out.println("Batata");
-                System.out.print("Quantidade: ");
-                int quantidadeB = pdv.nextInt();
-            } else if (fritura == 31) {
-                System.out.println("Palmito");
-                System.out.print("Quantidade: ");
-                int quantidadeP = pdv.nextInt();
-            } else {
-                System.out.println("Picles");
-                System.out.print("Quantidade: ");
-                int quantidadePi = pdv.nextInt();
-            }
-        }
+			if (fritura == 3) {
+				nomeFritura = "Batata";
+				System.out.printf("%s\n",nomeFritura);
+				System.out.print("Quantidade: ");
+				quantFritura = pdv.nextInt();
+				
+			} else if (fritura == 31) {
+				nomeFritura = "Palmito";
+				System.out.printf("%s\n",nomeFritura);
+				System.out.print("Quantidade: ");
+				quantFritura = pdv.nextInt();
+				
+			} else {
+				nomeFritura = "Bacon";
+				System.out.printf("%s\n",nomeFritura);
+				System.out.print("Quantidade: ");
+				quantFritura = pdv.nextInt();
+				
+			}
+		}
 
-        if (codigo == 11) {
-            System.out.println("\nResumo da Comanda: ");
-            System.out.println("Comanda Nº: " + comanda);
-            System.out.println("Cliente: " + nome);
-            System.out.println("Pedido:");
-            System.out.print(" - Item " + codigo + " | ");
-            System.out.println("X-Egg");
-            System.out.print("Quantidade: ");
-            int quantidadeH = pdv.nextInt();
+		if (codigo == 11) {
+			System.out.println("\nResumo da Comanda: ");
+			System.out.println("Comanda Nº: " + comanda);
+			System.out.println("Cliente: " + nome);
+			System.out.println("Pedido:");
+			System.out.print(" - Item " + codigo + " | ");
+			nomeHamburguer = "X-Egg";
+			System.out.printf("%s\n",nomeHamburguer);
+			System.out.print("Quantidade: ");
+			quantHamburg = pdv.nextInt();
 
-            System.out.print("Escolha Refresco 02 Suco 21 Refri 22 Água: ");
-            int refresco = pdv.nextInt();
+			System.out.print("Escolha Refresco 02 Suco 21 Refri 22 Água: ");
+			int refresco = pdv.nextInt();
 
-            if (refresco == 2) {
-                System.out.println("Suco");
-                System.out.print("Quantidade: ");
-                int quantidadeS = pdv.nextInt();
-            } else if (refresco == 21) {
-                System.out.println("Refri");
-                System.out.print("Quantidade: ");
-                int quantidadeR = pdv.nextInt();
-            } else {
-                System.out.println("Água");
-                System.out.print("Quantidade: ");
-                int quantidadeA = pdv.nextInt();
-            }
+			if (refresco == 2) {
+				nomeBebida = "Suco";
+				System.out.printf("%s\n",nomeBebida);
+				System.out.print("Quantidade: ");
+			    quantBebida = pdv.nextInt();
+			    
+			} else if (refresco == 21) {
+				nomeBebida = "Refrigerante";
+				System.out.printf("%s\n",nomeBebida);
+				System.out.print("Quantidade: ");
+				quantBebida = pdv.nextInt();
+				
+			} else {
+				nomeBebida = "Água";
+				System.out.printf("%s\n",nomeBebida);
+				System.out.print("Quantidade: ");
+				quantBebida = pdv.nextInt();
+			
+			}
 
-            System.out.print("Escolha Fritura 03 Batata 31 Palmito 32 Picles: ");
-            int fritura = pdv.nextInt();
+			System.out.print("Escolha Fritura 03 Batata 31 Palmito 32 Picles: ");
+			int fritura = pdv.nextInt();
 
-            if (fritura == 3) {
-                System.out.println("Batata");
-                System.out.print("Quantidade: ");
-                int quantidadeB = pdv.nextInt();
-            } else if (fritura == 31) {
-                System.out.println("Palmito");
-                System.out.print("Quantidade: ");
-                int quantidadeP = pdv.nextInt();
-            } else {
-                System.out.println("Picles");
-                System.out.print("Quantidade: ");
-                int quantidadePi = pdv.nextInt();
-            }
-        }
+			if (fritura == 3) {
+				nomeFritura = "Batata";
+				System.out.printf("%s\n",nomeFritura);
+				System.out.print("Quantidade: ");
+				quantFritura = pdv.nextInt();
+				
+			} else if (fritura == 31) {
+				nomeFritura = "Palmito";
+				System.out.printf("%s\n",nomeFritura);
+				System.out.print("Quantidade: ");
+				quantFritura = pdv.nextInt();
+				
+			} else {
+				nomeFritura = "Bacon";
+				System.out.printf("%s\n",nomeFritura);
+				System.out.print("Quantidade: ");
+				quantFritura = pdv.nextInt();
+			
+			}
+		}
 
-        if (codigo == 12) {
-            System.out.println("\nResumo da Comanda: ");
-            System.out.println("Comanda Nº: " + comanda);
-            System.out.println("Cliente: " + nome);
-            System.out.println("Pedido:");
-            System.out.print(" - Item " + codigo + " | ");
-            System.out.println("X-Tudo");
-            System.out.print("Quantidade: ");
-            int quantidadeH = pdv.nextInt();
+		if (codigo == 12) {
+			System.out.println("\nResumo da Comanda: ");
+			System.out.println("Comanda Nº: " + comanda);
+			System.out.println("Cliente: " + nome);
+			System.out.println("Pedido:");
+			System.out.print(" - Item " + codigo + " | ");
+			nomeHamburguer = "X-Tudo";
+			System.out.printf("%s\n",nomeHamburguer);
+			System.out.print("Quantidade: ");
+			quantHamburg = pdv.nextInt();
 
-            System.out.print("Escolha Refresco 02 Suco 21 Refri 22 Água: ");
-            int refresco = pdv.nextInt();
+			System.out.print("Escolha Refresco 02 Suco 21 Refri 22 Água: ");
+			int refresco = pdv.nextInt();
 
-            if (refresco == 2) {
-                System.out.println("Suco");
-                System.out.print("Quantidade: ");
-                int quantidadeS = pdv.nextInt();
-            } else if (refresco == 21) {
-                System.out.println("Refri");
-                System.out.print("Quantidade: ");
-                int quantidadeR = pdv.nextInt();
-            } else {
-                System.out.println("Água");
-                System.out.print("Quantidade: ");
-                int quantidadeA = pdv.nextInt();
-            }
+			if (refresco == 2) {
+				nomeBebida = "Suco";
+				System.out.printf("%s\n",nomeBebida);
+				System.out.print("Quantidade: ");
+				quantBebida = pdv.nextInt();
+				
+			} else if (refresco == 21) {
+				nomeBebida = "Refrigerante";
+				System.out.printf("%s",nomeBebida);
+				System.out.print("Quantidade: ");
+				quantBebida = pdv.nextInt();
+				
+			} else {
+				nomeBebida = "Água";
+				System.out.printf("%s",nomeBebida);
+				System.out.print("Quantidade: ");
+				quantBebida = pdv.nextInt();
+				
+			}
 
-            System.out.print("Escolha Fritura 03 Batata 31 Palmito 32 Picles: ");
-            int fritura = pdv.nextInt();
+			System.out.print("Escolha Fritura 03 Batata 31 Palmito 32 Picles: ");
+			int fritura = pdv.nextInt();
 
-            if (fritura == 3) {
-                System.out.println("Batata");
-                System.out.print("Quantidade: ");
-                int quantidadeB = pdv.nextInt();
-            } else if (fritura == 31) {
-                System.out.println("Palmito");
-                System.out.print("Quantidade: ");
-                int quantidadeP = pdv.nextInt();
-            } else {
-                System.out.println("Picles");
-                System.out.print("Quantidade: ");
-                int quantidadePi = pdv.nextInt();
-            }
-        }
+			if (fritura == 3) {
+				nomeFritura = "Batata";
+				System.out.printf("%s\n", nomeFritura);
+				System.out.print("Quantidade: ");
+				quantFritura = pdv.nextInt();
+				
+			} else if (fritura == 31) {
+				nomeFritura = "Palmito";
+				System.out.printf("%s\n", nomeFritura);
+				System.out.print("Quantidade: ");
+				quantFritura = pdv.nextInt();
+				
+			} else {
+				nomeFritura = "Bacon";
+				System.out.printf("%s\n", nomeFritura);
+				System.out.print("Quantidade: ");
+				quantFritura = pdv.nextInt();
+				
+			}
+		}
 
-        System.out.println("\nStatus do Pedido:");
-        System.out.println(" Preparando...");
-        Thread.sleep(4500); 
+		System.out.println("\nStatus do Pedido:");
+		Thread.sleep(500);
 
-        System.out.println(" Finalizando...");
-        Thread.sleep(3500);
+		System.out.println(" Preparando...");
+		Thread.sleep(500);
 
-        System.out.println(" Pedido pronto para retirada!");
-        Thread.sleep(2500);
+		System.out.println(" Finalizando...");
+		Thread.sleep(500);
 
-        System.out.println("\n=======================================");
-        System.out.println(" Comanda Nº: " + comanda);
-        System.out.println(" Cliente: " + nome);
-        System.out.println(" Obrigado por escolher nossa lanchonete!");
-        System.out.println("=========================================\n");
+		System.out.println(" Pedido pronto para retirada!");
+		Thread.sleep(500);
 
-        pdv.close();
-    }
+		System.out.println("\n=======================================");
+		System.out.println(" Comanda Nº: " + comanda);
+		System.out.println(" Cliente: " + nome);
+		System.out.printf(" Pedido: %s (%d) %s (%d) %s (%d)\n", nomeHamburguer,quantHamburg,nomeBebida,quantBebida,nomeFritura,quantFritura);
+		System.out.println(" Obrigado por escolher nossa lanchonete!");
+		System.out.println("=========================================\n");
+
+		pdv.close();
+	}
 }

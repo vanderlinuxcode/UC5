d2a7c7969e9169a2d40f57d8688805aae54dbb23
vanderlinuxcode <vanderlitecnologia@gmail.com>
2025-09-02 06:41:45 -0300

    Organização do código, melhorias

--- aula14.08.25/Mercadinho/src/Estoque/SistemaDeEstoque.java
+++ aula14.08.25/Mercadinho/src/Estoque/SistemaDeEstoque.java
@@ -1,33 +1,63 @@
 package Estoque;
 
-import java.util.Scanner;
+	// Superclasse Produto
+	public class Produto {
+		
+		// Declaração de atributos
+	    private String nome;
+	    private int quantidade;
+	    private double preco;
+	    private String codigoBarras;
+	    private String categoria;
+	    private boolean disponivel;
 
-public class SistemaDeEstoque {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	    //Construtor da classe principal
+	    public Produto(String nome, int quantidade, double preco, String codigoBarras, String categoria) {
+	        this.nome = nome;
+	        this.quantidade = quantidade;
+	        this.preco = preco;
+	        this.codigoBarras = codigoBarras;
+	        this.categoria = categoria;
+	        this.disponivel = quantidade > 0;
+	    }
+	    
+	    // Informações que serão exibidas após primeira chamada.
+	    public void exibirInformacoes() {
+	        System.out.println("=== INFORMAÇÕES DO PRODUTO ===");
+	        System.out.println("Nome: " + nome);
+	        System.out.println("Categoria: " + categoria);
+	        System.out.println("Código de Barras: " + codigoBarras);
+	        System.out.println("Preço: R$" + preco);
+	        System.out.println("Quantidade em estoque: " + quantidade);
+	        System.out.println("Disponível para venda: " + (disponivel ? "Sim" : "Não"));
+	        System.out.println("==============================");
+	    }
 
-        Produto produto = new Produto(
-            "Arroz Tipo 1",
-            50,
-            5.99,
-            "7891234567890",
-            "Alimentos"
-        );
+	    public void vender(int quantidadeVendida) {
+	        if (quantidadeVendida <= quantidade && quantidadeVendida > 0) {
+	            quantidade -= quantidadeVendida;
+	            System.out.println("Venda realizada: " + quantidadeVendida + " unidades.");
+	        } else {
+	            System.out.println(" Estoque insuficiente ou quantidade inválida.");
+	        }
+	        atualizarDisponibilidade();
+	    }
 
-        produto.exibirInformacoes();
+	    public void repor(int quantidadeReposicao) {
+	        if (quantidadeReposicao > 0) {
+	            quantidade += quantidadeReposicao;
+	            System.out.println("Reposição realizada: " + quantidadeReposicao + " unidades.");
+	        } else {
+	            System.out.println("Quantidade de reposição inválida.");
+	        }
+	        atualizarDisponibilidade();
+	    }
 
-        // Simular venda
-        System.out.print("\nDigite a quantidade para venda: ");
-        int venda = scanner.nextInt();
-        produto.vender(venda);
-        produto.exibirEstoqueAtual();
+	    private void atualizarDisponibilidade() {
+	        disponivel = quantidade > 0;
+	    }
 
-        // Simular reposição
-        System.out.print("\nDigite a quantidade para reposição: ");
-        int reposicao = scanner.nextInt();
-        produto.repor(reposicao);
-        produto.exibirEstoqueAtual();
-
-        scanner.close();
-    }
-}
+	    public void exibirEstoqueAtual() {
+	        System.out.println("Estoque atual: " + quantidade + " unidades.");
+	    }
+	}

80525dd7e2e0a2939662ec08e4af8d7658ff8d1e
vanderlinuxcode <vanderlitecnologia@gmail.com>
2025-09-18 21:26:09 -0300

    Update Conexao.java

--- SETEMBRO/SGBD/Atividade5/SistemaClientes/src/dao/Conexao.java
+++ SETEMBRO/SGBD/Atividade5/SistemaClientes/src/dao/Conexao.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String URL = "jdbc:mysql://172.16.9.133:3306/clientes_db";
+    private static final String URL = "jdbc:mysql://172.16.9.XXX:3306/clientes_db";
     private static final String USUARIO = "root";
     private static final String SENHA = "";
 
